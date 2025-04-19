9d94d43ba81109b430541f39185d36da4abe80e1
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2025-04-19 21:33:33 +0100

    CHANGE placeable items to be behind the player

--- scenes/entities/placeable_item.gd
+++ scenes/entities/placeable_item.gd
@@ -5,8 +5,16 @@ extends Node2D
 @onready var item_sprite: Sprite2D = $ItemSprite
 
 @export var item: Item
+var gifter: Gifter
+var walkable: bool
 
 func _ready() -> void:
+	print(item)
 	if item.block_line_of_sight:
-		area_2d.collision_layer = 6
+		self.collision_layer = 12
 	item_sprite.texture = item.texture
+	walkable = item.walkable
+
+func setup(item: Item, gifter: Gifter) -> void:
+	self.item = item
+	self.gifter = gifter
