157464ef70596f2335de5628fa6b2b1ed8667b54
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2025-04-19 21:32:00 +0100

    ADD door object

---
+++ scenes/entities/door.gd
@@ -0,0 +1,32 @@
+class_name Door
+extends StaticBody2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+enum STATES { OPEN, CLOSED, LOCKED }
+
+@export var state: STATES = STATES.LOCKED
+
+func unlock() -> void:
+	_update_state(STATES.OPEN)
+
+func lock() -> void:
+	_update_state(STATES.LOCKED)
+
+func _update_state(new_state: STATES) -> void:
+	if state == new_state:
+		return
+	
+	if state == STATES.LOCKED:
+		self.collision_layer = 4
+	
+	match new_state:
+		STATES.LOCKED:
+			sprite_2d.visible = true
+			self.collision_layer = 12
+		STATES.OPEN:
+			sprite_2d.visible = false
+		STATES.CLOSED:
+			sprite_2d.visible = true
+		
+	state = new_state
