cf7802a1f4d05799942046a006bb7e9163d48fb2
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2025-04-20 01:26:40 +0100

    ADD level 1 and 2 scenes

---
+++ scenes/levels/game.gd
@@ -0,0 +1,16 @@
+class_name Game
+extends Node2D
+
+signal goal_reached()
+
+func change_level(level: Level) -> void:
+	for child in self.get_children():
+		child.queue_free()
+	
+	var new_level = level.level_scene.instantiate()
+	self.add_child(new_level)
+	new_level.connect("goal_reached", handle_goal_reached)
+
+func handle_goal_reached():
+	goal_reached.emit()
+	
---
+++ scenes/levels/level.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+signal goal_reached()
+
+@export var goal: Door
+
+func _ready() -> void:
+	if goal:
+		goal.connect("state_changed", reached_goal)
+
+func reached_goal(new_state: Door.STATES):
+	prints("REACHED THE GOAL")
+	goal_reached.emit()
