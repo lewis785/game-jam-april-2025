cab109ed320b3e99776010271d2ea320fc665fab
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2025-04-20 15:38:46 +0100

    CHANGE improve ui look

--- scenes/ui/hud.gd
+++ scenes/ui/hud.gd
@@ -6,16 +6,14 @@ extends Control
 
 var item: Item
 
-func _ready() -> void:
-	if item == null:
-		visible = false
-
 func change_item(new_item: Item) -> void:
-	visible = true
 	item = new_item
 	item_texture.texture = item.texture
 	item_lable.text = item.name
 	
 func remove_item() -> void:
-	visible = false
 	item = null
+	if item_texture:
+		item_texture.texture = null
+	if item_lable:
+		item_lable.text = ""
